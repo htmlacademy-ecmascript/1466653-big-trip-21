0c9acb6bf79e0b11e23048bca2e8b56fe3ce4fd2
Goulshat <76894691+Goulshat@users.noreply.github.com>
2023-09-02 16:09:12 +0500

    created mock data and models for points, offers, destinations

--- src/presenter/trips-presenter.js
+++ src/presenter/trips-presenter.js
@@ -2,22 +2,25 @@ import SortFormView from '../view/sort-form-view.js';
 import EventsListView from '../view/events-list-view.js';
 import EventView from '../view/event-view.js';
 import EventEditView from '../view/event-edit-view.js';
-import {render} from '../render.js';
+import { render } from '../render.js';
 
 export default class TripsPresenter {
   eventsListComponent = new EventsListView();
 
-  constructor({container}) {
+  constructor({ container, pointsModel }) {
     this.mainContainer = container;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.points = [...this.pointsModel.getPoints()];
+
     render(new SortFormView(), this.mainContainer);
     render(this.eventsListComponent, this.mainContainer);
     render(new EventEditView(), this.eventsListComponent.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new EventView(), this.eventsListComponent.getElement());
+    for (let i = 0; i < this.points.length; i++) {
+      render(new EventView({ point: this.points[i] }), this.eventsListComponent.getElement());
     }
   }
 }
